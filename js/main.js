82c35212005dde85c289daca8a11f9d2c1902499
Werbete Kaue <kauebrxff@gmail.com>
2026-09-03 10:25:26 -0300

    feat: Adiciona a lógica para manipulação de submissão de formulário e integração com a API para salvar pensamentos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const fomularioPensamento = document.getElementById("pensamento-form")
+    fomularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+
 })
+
+async function manipularSubmissaoFormulario(event){
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos()
+    } catch (error) {
+        alert("Erro ao salvar pensamento")
+    }
+}
