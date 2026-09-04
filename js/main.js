74ac2817f3bff46c32338987f9462c8df4abe73c
Werbete Kaue <kauebrxff@gmail.com>
2026-09-04 11:16:18 -0300

    feat: Substitui fetch por axios nas funções de API e melhora o tratamento de erros na renderização de pensamentos

--- js/main.js
+++ js/main.js
@@ -2,34 +2,30 @@ import ui from "./ui.js"
 import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
-  ui.renderizarPensamentos()
+    ui.renderizarPensamentos()
 
-  const formularioPensamento = document.getElementById("pensamento-form")
-  const botaoCancelar = document.getElementById("botao-cancelar")
+    const fomularioPensamento = document.getElementById("pensamento-form")
+    const botaoCancelar = document.getElementById("botao-cancelar")
 
-  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
-  botaoCancelar.addEventListener("click", manipularCancelamento)
+    fomularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener("click", manipularCancelamento)
+
 })
 
-async function manipularSubmissaoFormulario(event) {
-  event.preventDefault()
-  const id = document.getElementById("pensamento-id").value
-  const conteudo = document.getElementById("pensamento-conteudo").value
-  const autoria = document.getElementById("pensamento-autoria").value
+async function manipularSubmissaoFormulario(event){
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
 
-  try { 
-    if(id) {
-      await api.editarPensamento({ id, conteudo, autoria })
-    } else {
-      await api.salvarPensamento({ conteudo, autoria })
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos()
+    } catch (error) {
+        alert("Erro ao salvar pensamento")
     }
-    ui.renderizarPensamentos()
-  }
-  catch {
-    alert("Erro ao salvar pensamento")
-  }
 }
 
-function manipularCancelamento() {
-  ui.limparFormulario()
+function manipularCancelamento(){
+    ui.limparFormulario()
 }
