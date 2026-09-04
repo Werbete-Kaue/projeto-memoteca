ad17b40ca4ddde6094e0c77a4b0a718e52bac46b
Werbete Kaue <kauebrxff@gmail.com>
2026-09-04 09:04:01 -0300

    fix: Corrige a formatação e o tratamento de erros na função buscarPensamentos

--- js/api.js
+++ js/api.js
@@ -1,13 +1,14 @@
 const api = {
-    async buscarPensamentos(){
-        try {
-            const response = await fetch("http://localhost:3000/pensamentos")
-            return await response.json()
-        } catch (error) {
-            alert("Erro ao buscar pensamentos")
-            throw error
-        }
-    },
+    async buscarPensamentos() {
+    try {
+    const response = await fetch('http://localhost:3000/pensamentos')
+    return await response.json()
+    }
+    catch {
+    alert('Erro ao buscar pensamentos')
+    throw error
+    }
+},
 
     async salvarPensamento(pensamento){
         try {
